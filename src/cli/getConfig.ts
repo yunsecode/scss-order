ff81d767950ed5a8e27ec48a3b9a35e20b968220
yunsecode09 <yunsecode@gmail.com>
2024-03-19 11:08:50 +0100

    feat: can config with config file

--- src/cli/getConfig.ts
+++ src/cli/getConfig.ts
@@ -1,12 +1,35 @@
+import * as fs from 'fs';
+
 import { Config } from "../utils";
 
 export function getConfig(rawArguments = process.argv.slice(2)): Config {
     let config: Config = {
         orderList: [],
-        tabSize: 2,
+        tabSize: 4,
         spaceBeforeClass: true,
     }
 
-    // console.log(rawArguments);
+    const configIndex = rawArguments.indexOf('--config');
+    const configFile = configIndex !== -1 ? rawArguments[configIndex + 1] : null;
+
+    fs.readFile(configFile, 'utf8', (err: NodeJS.ErrnoException | null, data: string) => {
+        if (err) {
+            console.error('Error reading file.', err);
+        }
+
+        const configFileData = JSON.parse(data)
+
+        if (configFileData.orderList) {
+            config.orderList = configFileData.orderList;
+        }
+        if (configFileData.tabSize) {
+            config.tabSize = configFileData.tabSize;
+        }
+        if (configFileData.spaceBeforeClass) {
+            config.spaceBeforeClass = configFileData.spaceBeforeClass;
+        }
+
+    });
+
     return config;
 }
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,5 @@
 export { default as orderProperties } from './order';
 export { Config } from "../interfaces/config";
 export { formatProperties } from './format'
+
+// TODO: ci / cd or pre commit hook to check package.json is upgraded or not
