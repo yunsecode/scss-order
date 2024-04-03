912e6fe81dc6eb424cd218d032bece3c3c84068b
yunsecode09 <yunsecode@gmail.com>
2024-04-03 17:33:26 +0200

    feat(cli): cli print msg

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -1,5 +1,6 @@
 import * as fs from 'fs';
 import * as path from 'path';
+import chalk = require('chalk');
 
 import { Config } from '../utils';
 import { orderProperties } from '../utils';
@@ -8,31 +9,14 @@ import { getConfig } from './getConfig';
 import { ERR_MSG_HEADER } from '../utils/constant';
 import { orderCheck } from '../utils/orderCheck';
 
-function  getFileInfo(filePath: string): Promise<string | null> {
-    return new Promise((resolve) => {
-        fs.stat(filePath, (error: NodeJS.ErrnoException | null, stats: fs.Stats | undefined) => {
-            if (error) {
-                resolve(null);
-            }
+const RUNNING_TEXT = ' RUNS ';
+const RUNNING = `${chalk.reset.inverse.yellow.bold(RUNNING_TEXT)}`;
 
-            if (stats && stats.isFile()) {
-                if (path.extname(filePath) === '.scss') {
-                    fs.readFile(filePath, 'utf8', (err: NodeJS.ErrnoException | null, data: string) => {
-                        if (err) {
-                            resolve(null);
-                        }
+const FAIL_TEXT = ' FAIL ';
+const FAIL = `${chalk.reset.inverse.red.bold(FAIL_TEXT)}`;
 
-                        resolve(data);
-                    });
-                } else {
-                    resolve(null);
-                }
-            } else {
-                resolve(null);
-            }
-        });
-    });
-}
+const PASS_TEXT = ' PASS ';
+const PASS = `${chalk.reset.inverse.green.bold(PASS_TEXT)}`;
 
 async function setPage(filePath:string, textToWrite: string): Promise<void> {
     fs.writeFile(filePath, textToWrite, (err) => {
@@ -42,27 +26,70 @@ async function setPage(filePath:string, textToWrite: string): Promise<void> {
     });
 }
 
-async function parseDirectory(config: Config): Promise<void> {
-    const directoryPath: string = './';
+async function findScssFiles(dir: string, allScssFiles: string[] = []): Promise<string[]> {
+    const dirents = await fs.promises.readdir(dir, { withFileTypes: true });
+    for (const dirent of dirents) {
+        const res = path.resolve(dir, dirent.name);
+        if (dirent.isDirectory()) {
+            await findScssFiles(res, allScssFiles);
+        } else if (res.endsWith('.scss')) {
+            allScssFiles.push(res);
+        }
+    }
+    return allScssFiles;
+}
 
-    try {
-        const files: string[] = await fs.promises.readdir(directoryPath);
+function drawProgressBar(percentage: number): void {
+    const total = 20;
+    const progressLength = Math.floor(percentage / 100 * total);
+    const progressBar = chalk.green('█').repeat(progressLength) + chalk.white('░').repeat(total - progressLength);
+    process.stdout.write(`\r[${progressBar}] ${percentage}%`);
+}
 
-        for (const file of files) {
-            const filePath: string = path.join(directoryPath, file);
+async function orderAllFiles(config: Config): Promise<void> {
+    const styleFileList: string[] = await findScssFiles('.');
 
-            const fileData: string | null = await getFileInfo(filePath);
-            if (!fileData) {
-                continue;
-            }
+    const currentDirPath = process.cwd();
+
+    let orderNum = 0;
+    const startTime: Date = new Date(); // 시작 시간 기록
+
+    for (const styleFilePath of styleFileList) {
+        const filePath = styleFilePath.replace(currentDirPath, '');
+        console.log(RUNNING, chalk.gray(path.dirname(filePath).slice(1) + '/') +path.basename(filePath));
+
+    }
+    for (const styleFilePath of styleFileList) {
+        const index = styleFileList.indexOf(styleFilePath);
+        const goUp = styleFileList.length - index;
+
+        try {
+            const fileData = await fs.promises.readFile(styleFilePath, 'utf8');
             const res: string[] = orderProperties(config, fileData);
             const asd: string = formatProperties(config, res);
-            setPage(filePath, asd);
-
+            setPage(styleFilePath, asd);
+            process.stdout.write('\r\x1b[A'.repeat(goUp));
+            process.stdout.write(PASS);
+            process.stdout.write('\n'.repeat(goUp));
+            orderNum += 1;
+        } catch (error) {
+            process.stdout.write('\r\x1b[A'.repeat(goUp));
+            process.stdout.write(FAIL);
+            process.stdout.write('\n'.repeat(goUp));
+        } finally {
+            process.stdout.write('\x1b[2K');
+            drawProgressBar((index + 1) / styleFileList.length * 100);
         }
-    } catch (err) {
-        console.error(ERR_MSG_HEADER, err);
     }
+    process.stdout.write('\x1b[2K\r\n');
+    const endTime: Date = new Date(); // 종료 시간 기록
+    const elapsedTime: number = endTime.getTime() - startTime.getTime();
+    const elapsedTimeSec = Math.round((elapsedTime / 1000) * 100) / 100;
+
+    console.log('Order Files:\t' + chalk.green(`${orderNum} passed`) +  ` ${styleFileList.length} total`);
+    console.log('Time:\t\t' + elapsedTimeSec + 's');
+    
+    // process.stdout.write('\n');
 }
 
 export async function run(args = process.argv.slice(2)): Promise<void> {
@@ -79,5 +106,6 @@ export async function run(args = process.argv.slice(2)): Promise<void> {
         }
         return;
     }
-    await parseDirectory(config);
+    await orderAllFiles(config);
+    // await parseDirectory(config);
 }
